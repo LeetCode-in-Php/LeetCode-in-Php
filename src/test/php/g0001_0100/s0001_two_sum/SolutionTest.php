40dba01df4d3d46a176f9b56add84e94485edbd7
Valentyn Kolesnikov <javadev75@gmail.com>
2023-11-29 06:06:54 +0200

    Update SolutionTest.php

--- src/test/php/g0001_0100/s0001_two_sum/SolutionTest.php
+++ src/test/php/g0001_0100/s0001_two_sum/SolutionTest.php
@@ -6,18 +6,18 @@ use PHPUnit\Framework\TestCase;
 
 class SolutionTest extends TestCase {
     public function testTwoSum() {
-        $this->assertEquals([0, 1], (new Solution())->twoSum([2, 7, 11, 15], 9));
+        $this->assertEquals([0, 1], (new \g0001_0100\s0001_two_sum\Solution())->twoSum([2, 7, 11, 15], 9));
     }
 
     public function testTwoSum2() {
-        $this->assertEquals([1, 2], (new Solution())->twoSum([3, 2, 4], 6));
+        $this->assertEquals([1, 2], (new \g0001_0100\s0001_two_sum\Solution())->twoSum([3, 2, 4], 6));
     }
 
     public function testTwoSum3() {
-        $this->assertEquals([0, 1], (new Solution())->twoSum([3, 3], 6));
+        $this->assertEquals([0, 1], (new \g0001_0100\s0001_two_sum\Solution())->twoSum([3, 3], 6));
     }
 
     public function testTwoSum4() {
-        $this->assertEquals([-1, -1], (new Solution())->twoSum([3, 3], 7));
+        $this->assertEquals([-1, -1], (new \g0001_0100\s0001_two_sum\Solution())->twoSum([3, 3], 7));
     }
 }
