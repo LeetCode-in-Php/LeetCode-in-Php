22cd1bd88791e3ee03480c61d10f01d632bc931c
Valentyn Kolesnikov <javadev75@gmail.com>
2023-11-29 19:49:27 +0200

    Fixed tests

--- src/test/php/g0001_0100/s0001_two_sum/SolutionTest.php
+++ src/test/php/g0001_0100/s0001_two_sum/SolutionTest.php
@@ -6,18 +6,18 @@ use PHPUnit\Framework\TestCase;
 
 class SolutionTest extends TestCase {
     public function testTwoSum() {
-        assertThat((new Solution())->twoSum([2, 7, 11, 15], 9), equalTo([0, 1]));
+        $this->assertThat((new Solution())->twoSum([2, 7, 11, 15], 9), equalTo([0, 1]));
     }
 
     public function testTwoSum2() {
-        assertThat((new Solution())->twoSum([3, 2, 4], 6), equalTo([1, 2]));
+        $this->assertThat((new Solution())->twoSum([3, 2, 4], 6), equalTo([1, 2]));
     }
 
     public function testTwoSum3() {
-        assertThat((new Solution())->twoSum([3, 3], 6), equalTo([0, 1]));
+        $this->assertThat((new Solution())->twoSum([3, 3], 6), equalTo([0, 1]));
     }
 
     public function testTwoSum4() {
-        assertThat((new Solution())->twoSum([3, 3], 7), equalTo([-1, -1]));
+        $this->assertThat((new Solution())->twoSum([3, 3], 7), equalTo([-1, -1]));
     }
 }
