45373c2635a38745851b20a2b3abcde3002a08d1
Valentyn Kolesnikov <javadev75@gmail.com>
2023-11-29 20:17:17 +0200

    Improved test

--- src/test/php/g0001_0100/s0002_add_two_numbers/SolutionTest.php
+++ src/test/php/g0001_0100/s0002_add_two_numbers/SolutionTest.php
@@ -6,29 +6,29 @@ use PHPUnit\Framework\TestCase;
 use leetcode\com_github_leetcode\LinkedListUtils;
 use leetcode\com_github_leetcode\ListNode;
 
-class SolutionTest {
+class SolutionTest extends TestCase {
     public function testAddTwoNumbers() {
         $listNode1 = LinkedListUtils::constructLinkedList([2, 4, 3]);
         $listNode2 = LinkedListUtils::constructLinkedList([5, 6, 4]);
-        assertThat(
+        $this->assertThat(
             (new Solution())->addTwoNumbers($listNode1, $listNode2)->toString(),
-            equalTo("7, 0, 8")
+            $this->equalTo("7, 0, 8")
         );
     }
 
     public function testAddTwoNumbers2() {
-        assertThat(
+        $this->assertThat(
             (new Solution())->addTwoNumbers(new ListNode(0), new ListNode(0))->toString(),
-            equalTo("0")
+            $this->equalTo("0")
         );
     }
 
     public function testAddTwoNumbers3() {
         $listNode1 = LinkedListUtils::constructLinkedList([9, 9, 9, 9, 9, 9, 9]);
         $listNode2 = LinkedListUtils::constructLinkedList([9, 9, 9, 9]);
-        assertThat(
+        $this->assertThat(
             (new Solution())->addTwoNumbers($listNode1, $listNode2)->toString(),
-            equalTo("8, 9, 9, 9, 0, 0, 0, 1")
+            $this->equalTo("8, 9, 9, 9, 0, 0, 0, 1")
         );
     }
 }
