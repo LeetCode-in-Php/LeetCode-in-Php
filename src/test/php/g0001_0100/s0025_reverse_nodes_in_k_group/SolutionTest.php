038c57e3f5c2d35d8213c0c5cca43188a0a55f39
Valentyn Kolesnikov <javadev75@gmail.com>
2023-12-08 05:36:20 +0200

    Fixed tests

--- src/test/php/g0001_0100/s0025_reverse_nodes_in_k_group/SolutionTest.php
+++ src/test/php/g0001_0100/s0025_reverse_nodes_in_k_group/SolutionTest.php
@@ -9,11 +9,11 @@ use PHPUnit\Framework\TestCase;
 class SolutionTest extends TestCase {
     public function testReverseKGroup() {
         $head = LinkedListUtils::constructLinkedList([1, 2, 3, 4, 5]);
-        $this->assertEquals("2, 1, 4, 3, 5", (new Solution())->reverseKGroup($head, 2).toString());
+        $this->assertEquals("2, 1, 4, 3, 5", (new Solution())->reverseKGroup($head, 2)->toString());
     }
 
     public function testReverseKGroup2() {
         $head = LinkedListUtils::constructLinkedList([1, 2, 3, 4, 5]);
-        $this->assertEquals("3, 2, 1, 4, 5", (new Solution())->reverseKGroup($head, 3).toString());
+        $this->assertEquals("3, 2, 1, 4, 5", (new Solution())->reverseKGroup($head, 3)->toString());
     }
 }
