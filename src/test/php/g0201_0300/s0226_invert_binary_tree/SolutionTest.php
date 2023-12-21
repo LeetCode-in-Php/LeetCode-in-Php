1338831baf03c22ddefa36f7d31bfebcadaf4f0d
Valentyn Kolesnikov <javadev75@gmail.com>
2023-12-21 19:43:33 +0200

    Fixed test

--- src/test/php/g0201_0300/s0226_invert_binary_tree/SolutionTest.php
+++ src/test/php/g0201_0300/s0226_invert_binary_tree/SolutionTest.php
@@ -8,11 +8,11 @@ use leetcode\com_github_leetcode\TreeUtils;
 class SolutionTest extends TestCase {
     public function testInvertTree() {
         $root = TreeUtils::constructBinaryTree(array(4, 2, 7, 1, 3, 6, 9));
-        $this->assertEquals("4,7,9,6,2,3,1", (new Solution())->invertTree($root)->toString());
+        $this->assertEquals("4,7,9,6,2,3,1", (new Solution())->invertTree($root));
     }
 
     public function testInvertTree2() {
         $root = TreeUtils::constructBinaryTree(array(2, 1, 3));
-        $this->assertEquals("2,3,1", (new Solution())->invertTree($root)->toString());
+        $this->assertEquals("2,3,1", (new Solution())->invertTree($root));
     }
 }
