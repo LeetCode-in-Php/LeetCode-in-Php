6e84b9a3c999be927932a3f636be6de0f705aff6
Valentyn Kolesnikov <javadev75@gmail.com>
2023-12-24 04:14:39 +0200

    Fixed 437

--- src/main/php/g0401_0500/s0437_path_sum_iii/Solution.php
+++ src/main/php/g0401_0500/s0437_path_sum_iii/Solution.php
@@ -3,7 +3,7 @@
 namespace leetcode\g0401_0500\s0437_path_sum_iii;
 
 // #Medium #Top_100_Liked_Questions #Depth_First_Search #Tree #Binary_Tree #Level_2_Day_7_Tree
-// #Big_O_Time_O(n)_Space_O(n) #2023_12_24_Time_13_ms_(81.25%)_Space_20.1_MB_(43.75%)
+// #Big_O_Time_O(n)_Space_O(n) #2023_12_24_Time_11_ms_(100.00%)_Space_20.7_MB_(12.50%)
 
 /**
  * Definition for a binary tree node.
@@ -19,26 +19,31 @@ namespace leetcode\g0401_0500\s0437_path_sum_iii;
  * }
  */
 class Solution {
+    private function countPaths($node, &$count, $prefixSum, $targetSum) {
+        if ($node === null) {
+            return 0;
+        }
+        if (isset($count[$prefixSum])) {
+            $count[$prefixSum]++;
+        } else {
+            $count[$prefixSum] = 1;
+        }
+        $prefixSum += $node->val;
+        $ans = $count[$prefixSum - $targetSum]
+            + $this->countPaths($node->left, $count, $prefixSum, $targetSum)
+            + $this->countPaths($node->right, $count, $prefixSum, $targetSum);
+        $prefixSum -= $node->val;
+        $count[$prefixSum]--;
+        return $ans;
+    }
+
     /**
      * @param TreeNode $root
      * @param Integer $targetSum
      * @return Integer
      */
-    public function pathSum(?TreeNode $root, int $targetSum, int $cursumm = 0, array &$prev = []): int {
-        if (!$root) {
-            return 0;
-        }
-        $count = 0;
-        $cursumm += $root->val;
-        if ($cursumm === $targetSum) {
-            $count++;
-        }
-        $count += $prev[$cursumm - $targetSum] ?? 0;
-        $prev[$cursumm] = 1 + $prev[$cursumm] ?? 0;
-
-        $count += $this->pathSum($root->left, $targetSum, $cursumm, $prev) +
-            $this->pathSum($root->right, $targetSum, $cursumm, $prev);
-        $prev[$cursumm]--;
-        return $count;
+    public function pathSum($root, $targetSum) {
+        $count = [];
+        return $this->countPaths($root, $count, 0, $targetSum);
     }
 }
